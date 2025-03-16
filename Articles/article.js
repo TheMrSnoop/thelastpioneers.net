063d3618965edbace2058694591d98d6e8364a11
Snoop <thomas.k4209@gmail.com>
2025-03-16 12:22:29 -0500

    Style Tweaks

--- Articles/article.js
+++ Articles/article.js
@@ -1,6 +1,10 @@
 window.addEventListener("scroll", () => {
     const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
     const ArticleNavBar = document.getElementById("ArticleNavbar");
+    const NavProgressBar = document.getElementById("NavBarProgressBar");
+
+    NavProgressBar.style.width = `${(scrollTop / docHeight) * 100}vw`;
 
     if (scrollTop > 450) {
         ArticleNavBar.classList.remove("NavBarBackgroundTransparent");
