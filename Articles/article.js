73114f81407f1fff09a195ab9a24579686cfd84c
Snoop <thomas.k4209@gmail.com>
2025-04-07 18:03:53 -0500

    Added proper mobile support for the articles, and should have improved the homepage performance.

--- Articles/article.js
+++ Articles/article.js
@@ -1,5 +1,54 @@
+function gcd(a, b)
+{
+    while (b)
+    {
+        [a, b] = [b, a % b];
+    }
+    return a;
+}
+
+function findRatio(numerator, denominator)
+{
+    const commonDivisor = gcd(numerator, denominator)
+    const simplifiedNumerator = numerator / commonDivisor;
+    const simplifiedDenominator = denominator / commonDivisor;
+
+    return [simplifiedNumerator, simplifiedDenominator]
+}
+
+function MobilizeElement(array, className)
+{
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        element.className = "";
+        element.classList.add(className);
+    }
+}
+
+function AdjustForDisplayRatio()
+{
+    const baseHeight = window.innerHeight;
+    const baseWidth = window.innerWidth;
+
+    const paragraphs = document.getElementsByTagName("p");
+    const ArticlesImages = document.getElementsByClassName("ArticleImage")
+    const Heading_Fours = document.getElementsByTagName("h4");
+    
+    ratio = (Math.round((baseWidth / baseHeight) * 100)) / 100
+
+    console.log(ratio)
+
+    if (ratio < 0.85)
+    {
+        MobilizeElement(paragraphs, "P_Mobile");
+        MobilizeElement(ArticlesImages, "Mobile_Image");
+        MobilizeElement(Heading_Fours, "h4_Mobile")
+    }
+}
+
 window.addEventListener("scroll", () => {
     const scrollTop = window.scrollY;
+
     const docHeight = document.documentElement.scrollHeight - window.innerHeight;
     const ArticleNavBar = document.getElementById("ArticleNavbar");
     const NavProgressBar = document.getElementById("NavBarProgressBar");
@@ -13,4 +62,9 @@ window.addEventListener("scroll", () => {
         ArticleNavBar.classList.remove("NavBarBackground");
         ArticleNavBar.classList.add("NavBarBackgroundTransparent");
     }
+
+    //const [horizontalRatio, verticalRatio] = findRatio(baseWidth, baseHeight)
+
+    AdjustForDisplayRatio();
+
 });
